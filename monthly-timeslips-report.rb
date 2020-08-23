dfbcbc7054cfb7caf696a3e44f90670b5d18aeab
James Mead <james@floehopper.org>
2020-08-23 11:32:18 +0100

    Update list of users in monthly-timeslips-report

--- monthly-timeslips-report.rb
+++ monthly-timeslips-report.rb
@@ -36,8 +36,10 @@ def get_resources(name, filters = {})
 end
 
 users = [
+  OpenStruct.new(first_name: 'Ben', url: 'https://api.freeagent.com/v2/users/580257'),
+  OpenStruct.new(first_name: 'Chris L', url: 'https://api.freeagent.com/v2/users/485461'),
+  OpenStruct.new(first_name: 'Chris R', url: 'https://api.freeagent.com/v2/users/32469'),
   OpenStruct.new(first_name: 'James', url: 'https://api.freeagent.com/v2/users/7474'),
-  OpenStruct.new(first_name: 'Chris', url: 'https://api.freeagent.com/v2/users/32469'),
 ]
 
 results = {}
