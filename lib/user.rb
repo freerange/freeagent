e0239986ae0d2ecb14c69ab4e1e9978749bb722c
James Mead <james@floehopper.org>
2021-06-04 12:15:03 +0100

    Add new users to list

--- lib/user.rb
+++ lib/user.rb
@@ -5,6 +5,10 @@ class User
       OpenStruct.new(first_name: 'Chris L', url: 'https://api.freeagent.com/v2/users/485461'),
       OpenStruct.new(first_name: 'Chris R', url: 'https://api.freeagent.com/v2/users/32469'),
       OpenStruct.new(first_name: 'James', url: 'https://api.freeagent.com/v2/users/7474'),
+      OpenStruct.new(first_name: 'Chris P', url: 'https://api.freeagent.com/v2/users/917229'),
+      OpenStruct.new(first_name: 'Joel', url: 'https://api.freeagent.com/v2/users/960485'),
+      OpenStruct.new(first_name: 'Russell', url: 'https://api.freeagent.com/v2/users/960479'),
+      OpenStruct.new(first_name: 'Moira', url: 'https://api.freeagent.com/v2/users/1004243'),
     ]
   end
 end
